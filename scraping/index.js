09cf10f81c6cf4a64881a1f06ce7e670bfa5b566
josecarreno <ing.jcarreno@gmail.com>
2023-01-12 00:33:20 -0500

    optimaze

--- scraping/index.js
+++ scraping/index.js
@@ -1,34 +1,75 @@
-import * as cheerio from "cheerio";
+import * as cheerio from 'cheerio'
+const URl = {
+  learderboard: 'https://kingsleague.pro/estadisticas/clasificacion/'
+}
 
-const res = await fetch("https://kingsleague.pro/estadisticas/");
-const html = await res.text();
+const scrape = async (url) => {
+  const res = await fetch(url)
+  const html = await res.text()
+  return cheerio.load(html)
+}
 
-const $ = cheerio.load(html);
+async function getLearderboard () {
+  const $ = await scrape(URl.learderboard)
+  const $rows = $('table tbody tr')
 
-$("table tbody tr").each((index, el) => {
-  // console.log($(el).text());
-  const rawTeam = $(el).find(".fs-table-text_3").text();
-  const rawVictories = $(el).find(".fs-table-text_4").text();
-  const rawLoses = $(el).find(".fs-table-text_5").text();
-  const rawGoalsScored = $(el).find(".fs-table-text_6").text();
-  const rawGoalsConceded = $(el).find(".fs-table-text_7").text();
-  const rawCardsYellow = $(el).find(".fs-table-text_8").text();
-  const rawCardsRed = $(el).find(".fs-table-text_9").text();
+  // const LEARDERBOARD_SELECTORS_PREFIX = $('table tbody tr')
 
-  console.log({
-    rawTeam,
-    rawVictories,
-  });
-});
+  const LEARDERBOARD_SELECTORS = {
+    team: '.fs-table-text_3',
+    wins: '.fs-table-text_4',
+    loses: '.fs-table-text_5',
+    scoredGoals: '.fs-table-text_6',
+    concededGoals: '.fs-table-text_7',
+    cardsYellow: '.fs-table-text_8',
+    cardsRed: '.fs-table-text_9'
+  }
 
-const learderboard = [
-  {
-    team: "Team 1",
-    wins: 0,
-    loses: 0,
-    goalsScored: 0,
-    goalsConceded: 0,
-    cardsYellow: 0,
-    cardsRed: 0,
-  },
-];
+  const clearText = text => text
+    .replace(/\t|\n|\s:/g, '')
+    .replace(/.*:/g, ' ')
+    .trim()
+
+  const learderBoardSelectorEntries = Object.entries(LEARDERBOARD_SELECTORS)
+
+  $rows.each((index, el) => {
+    const learderBoardEntries = learderBoardSelectorEntries.map(([key, selector]) => {
+      // const selector = `${LEARDERBOARD_SELECTORS_PREFIX} ${specificSelector}`
+      const rawValue = $(el).find(selector).text()
+      const value = clearText(rawValue)
+      return [key, value]
+    })
+    console.log(Object.fromEntries(learderBoardEntries))
+    // const rawTeam = $el.find('.fs-table-text_3').text()
+    // const rawVictories = $el.find('.fs-table-text_4').text()
+    // const rawLoses = $el.find('.fs-table-text_5').text()
+    // const rawGoalsScored = $el.find('.fs-table-text_6').text()
+    // const rawGoalsConceded = $el.find('.fs-table-text_7').text()
+    // const rawCardsYellow = $el.find('.fs-table-text_8').text()
+    // const rawCardsRed = $el.find('.fs-table-text_9').text()
+
+  // console.log(
+  //   clearText(rawTeam),
+  //   clearText(rawVictories),
+  //   clearText(rawLoses),
+  //   clearText(rawGoalsScored),
+  //   clearText(rawGoalsConceded),
+  //   clearText(rawCardsYellow),
+  //   clearText(rawCardsRed)
+  // )
+  })
+}
+
+await getLearderboard()
+
+// const learderboard = [
+//   {
+//     team: 'Team 1',
+//     wins: 0,
+//     loses: 0,
+//     goalsScored: 0,
+//     goalsConceded: 0,
+//     cardsYellow: 0,
+//     cardsRed: 0
+//   }
+// ]
