81a3330c78c6f574ebdea606205f43c602b822ef
josecarreno <ing.jcarreno@gmail.com>
2023-01-17 22:30:13 -0500

    scraping listo

--- scraping/index.js
+++ scraping/index.js
@@ -1,4 +1,7 @@
 import * as cheerio from 'cheerio'
+import { writeFile } from 'node:fs/promises'
+import path from 'node:path'
+
 const URl = {
   learderboard: 'https://kingsleague.pro/estadisticas/clasificacion/'
 }
@@ -16,13 +19,13 @@ async function getLearderboard () {
   // const LEARDERBOARD_SELECTORS_PREFIX = $('table tbody tr')
 
   const LEARDERBOARD_SELECTORS = {
-    team: '.fs-table-text_3',
-    wins: '.fs-table-text_4',
-    loses: '.fs-table-text_5',
-    scoredGoals: '.fs-table-text_6',
-    concededGoals: '.fs-table-text_7',
-    cardsYellow: '.fs-table-text_8',
-    cardsRed: '.fs-table-text_9'
+    team: { selector: '.fs-table-text_3', typeOf: 'string' },
+    wins: { selector: '.fs-table-text_4', typeOf: 'number' },
+    loses: { selector: '.fs-table-text_5', typeOf: 'number' },
+    scoredGoals: { selector: '.fs-table-text_6', typeOf: 'number' },
+    concededGoals: { selector: '.fs-table-text_7', typeOf: 'number' },
+    cardsYellow: { selector: '.fs-table-text_8', typeOf: 'number' },
+    cardsRed: { selector: '.fs-table-text_9', typeOf: 'number' }
   }
 
   const clearText = text => text
@@ -31,15 +34,20 @@ async function getLearderboard () {
     .trim()
 
   const learderBoardSelectorEntries = Object.entries(LEARDERBOARD_SELECTORS)
-
+  const learderboard = []
   $rows.each((index, el) => {
-    const learderBoardEntries = learderBoardSelectorEntries.map(([key, selector]) => {
+    const learderBoardEntries = learderBoardSelectorEntries.map(([key, { selector, typeOf }]) => {
       // const selector = `${LEARDERBOARD_SELECTORS_PREFIX} ${specificSelector}`
       const rawValue = $(el).find(selector).text()
-      const value = clearText(rawValue)
+      const cleanValue = clearText(rawValue)
+      // transformandoa numero
+      // const value = Number.isNaN(Number(cleanValue)) ? cleanValue : Number(cleanValue)
+      const value = typeOf === 'number' ? Number(cleanValue) : cleanValue
       return [key, value]
     })
-    console.log(Object.fromEntries(learderBoardEntries))
+    learderboard.push(Object.fromEntries(learderBoardEntries))
+
+    // console.log(Object.fromEntries(learderBoardEntries))
     // const rawTeam = $el.find('.fs-table-text_3').text()
     // const rawVictories = $el.find('.fs-table-text_4').text()
     // const rawLoses = $el.find('.fs-table-text_5').text()
@@ -48,19 +56,24 @@ async function getLearderboard () {
     // const rawCardsYellow = $el.find('.fs-table-text_8').text()
     // const rawCardsRed = $el.find('.fs-table-text_9').text()
 
-  // console.log(
-  //   clearText(rawTeam),
-  //   clearText(rawVictories),
-  //   clearText(rawLoses),
-  //   clearText(rawGoalsScored),
-  //   clearText(rawGoalsConceded),
-  //   clearText(rawCardsYellow),
-  //   clearText(rawCardsRed)
-  // )
+    // console.log(
+    //   clearText(rawTeam),
+    //   clearText(rawVictories),
+    //   clearText(rawLoses),
+    //   clearText(rawGoalsScored),
+    //   clearText(rawGoalsConceded),
+    //   clearText(rawCardsYellow),
+    //   clearText(rawCardsRed)
+    // )
   })
+  return learderboard
 }
 
-await getLearderboard()
+const learderboard = await getLearderboard()
+const filePath = path.join(process.cwd(), './db', 'learderboard.json')
+
+console.log(filePath)
+await writeFile(filePath, JSON.stringify(learderboard, null, 2), 'utf-8')
 
 // const learderboard = [
 //   {
