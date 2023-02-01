ba0dd13347b64aada94b45e93285669e8d95e9eb
josecarreno <ing.jcarreno@gmail.com>
2023-01-31 21:43:35 -0500

    scraping mvp

---
+++ scraping/mvp.js
@@ -0,0 +1,59 @@
+/**
+ * writeDBFile: fichro para escribir el archivo .json.
+ * TEAMS: para leer toda la info que tiene los equipos
+ */
+import { TEAMS, writeDBFile } from '../db/index.js'
+import { URLS, scrape, clearText } from './utils.js'
+
+const MVP_SELECTORS = {
+	team: { selector: '.fs-table-text_3', typeOf: 'string' },
+	playerName: { selector: '.fs-table-text_4', typeOf: 'string' },
+	gamesPlayed: { selector: '.fs-table-text_5', typeOf: 'number' },
+	mvps: { selector: '.fs-table-text_6', typeOf: 'number' }
+}
+
+async function getMVPList () {
+	// escrapeando la informacion de la pagina  https://kingsleague.pro/estadisticas/mvp/
+	const $ = await scrape(URLS.mvp)
+	const $rows = $('table tbody tr')
+
+	const getImageFromTeam = ({ name }) => {
+		const { image } = TEAMS.find(
+			(team) => team.name === name
+		)
+		return image
+	}
+	// convierte un objeto a un arraye de [key, value]
+	const mvpSelectorEntries = Object.entries(MVP_SELECTORS)
+	const mvpList = []
+
+	$rows.each((index, el) => {
+		// iterando el Array
+		const mvpEntries = mvpSelectorEntries.map(
+			([key, { selector, typeOf }]) => {
+				const rawValue = $(el).find(selector).text()
+				// limpiar el string, espacio que tiene en el HTML
+				const cleanedValue = clearText(rawValue)
+
+				const value = typeOf === 'number' ? Number(cleanedValue) : cleanedValue
+
+				return [key, value]
+			}
+		)
+		// convertir el array en un objeto
+		const { team: teamName, ...mvpData } = Object.fromEntries(mvpEntries)
+		const image = getImageFromTeam({ name: teamName })
+
+		mvpList.push({
+			...mvpData,
+			rank: index + 1,
+			team: teamName,
+			image
+		})
+	})
+	return mvpList
+}
+
+const mvpList = await getMVPList()
+// escribiendo en el fichero
+await writeDBFile('mvpList', mvpList)
